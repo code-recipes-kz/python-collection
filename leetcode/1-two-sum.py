389fb45dd81e375fc7693850c048c56d30f328e6
PunkMachine <alex_rassydihin@mail.ru>
2023-03-11 18:47:08 +0600

    upd: переменную для теста.

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         num_map = {}
-        for i in range(len(nums)):
-            num = nums[i]
+        for i in range(len(numbers)):
+            num = numbers[i]
             complement = target - num
             if complement in num_map:
                 return [num_map[complement], i]

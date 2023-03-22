ca2b88d870d4e912cd337a73440abe4ac9266775
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-03-22 13:02:17 +0600

    add: ещё больше тасков.

--- utilities/generate-filename.py
+++ utilities/generate-filename.py
@@ -8,5 +8,5 @@ def transform_string(s):
     s = re.sub(r'(-{2,})', '-', s)  # Убираем повторяющиеся символы "-"
     return s
 
-s = transform_string('Find The Parity Outlier')
+s = transform_string('Take a Ten Minutes Walk')
 print(s)
